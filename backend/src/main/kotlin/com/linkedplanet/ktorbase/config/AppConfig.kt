6169149295bdafc0a34d370364422816164f1a2e
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2021-04-30 17:59:02 +0200

    make it much easier to just run the application by removing a lot of config and setting some defaults

--- backend/src/main/kotlin/com/linkedplanet/ktorbase/config/AppConfig.kt
+++ backend/src/main/kotlin/com/linkedplanet/ktorbase/config/AppConfig.kt
@@ -5,7 +5,6 @@ import com.typesafe.config.ConfigFactory
 object AppConfig {
 
     private val config = ConfigFactory.load().getConfig("app")
-    val baseUrl: String = config.getString("baseUrl")
     val title: String = config.getString("title")
     val bannerBackgroundColor: String = config.getString("bannerBackgroundColor")
     val bannerMenuBackgroundColor: String = config.getString("bannerMenuBackgroundColor")
