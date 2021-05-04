d7e1a168efe5d441b2a5c78fe75f5725ed8da59e
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2021-05-04 15:33:36 +0200

    remove obsolete code from common module

--- common/src/commonMain/kotlin/com/linkedplanet/ktorbase/routes/Responses.kt
+++ common/src/commonMain/kotlin/com/linkedplanet/ktorbase/routes/Responses.kt
@@ -3,5 +3,4 @@ package com.linkedplanet.ktorbase.routes
 import com.linkedplanet.ktorbase.model.Config
 import com.linkedplanet.ktorbase.model.Session
 
-
 data class SessionResponse(val session: Session, val config: Config)
