2ebcf29fe52059a2aeb86df7901b908e7d1405c6
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2022-11-30 17:46:21 +0100

    set version from git tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
-import org.gradle.util.GradleVersion
-
 println("Gradle Version: " + GradleVersion.current().toString())
 println("Java Version: " + JavaVersion.current().toString())
 
-group = "com.linked-planet"
-version = "0.1.0-SNAPSHOT"
-
-ext.set("kotlinVersion", "1.7.20")
-ext.set("jvmTarget", "1.8") // ktor prevents compile-time 11, see: https://youtrack.jetbrains.com/issue/KTOR-619
-
 plugins {
     kotlin("multiplatform") version "1.7.20" apply false
+
+    // derive gradle version from git tag
+    id("pl.allegro.tech.build.axion-release") version "1.14.3"
 
     // provide & configure tasks: dependencyUpdates, useLatestVersions
     id("com.github.ben-manes.versions") version "0.44.0"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
+
+group = "com.linked-planet"
+version = scmVersion.version
+
+ext.set("kotlinVersion", "1.7.20")
 
 allprojects {
     repositories {
