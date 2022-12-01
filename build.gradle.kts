ddc83b5da55114fafc5f0909ec6d62284d01ca08
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2022-12-01 12:49:04 +0100

    remove use-latest-versions gradle plugin as it does not work

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,9 @@ plugins {
     // derive gradle version from git tag
     id("pl.allegro.tech.build.axion-release") version "1.14.3"
 
-    // provide & configure tasks: dependencyUpdates, useLatestVersions
+    // provide & configure dependencyUpdates
     id("com.github.ben-manes.versions") version "0.44.0"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
-    id("se.patrikerdes.use-latest-versions") version "0.2.18"
 }
 
 group = "com.linked-planet"
