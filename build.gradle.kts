7dbb017ea2d724589b51841016ef328cd3a5d2c7
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2023-11-15 10:53:42 +0100

    upgrade axion-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
-println("Gradle Version: " + GradleVersion.current().toString())
-println("Java Version: " + JavaVersion.current().toString())
-
 plugins {
     kotlin("multiplatform") version "1.7.22" apply false
 
     // derive gradle version from git tag
-    id("pl.allegro.tech.build.axion-release") version "1.14.3"
+    id("pl.allegro.tech.build.axion-release") version "1.15.0"
 
     // provide & configure dependencyUpdates
-    id("com.github.ben-manes.versions") version "0.44.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
 }
 
